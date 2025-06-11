cc7cb10aba6222b5482955a6a34c814302ed1bb8
Tom Wardill <tom@howrandom.net>
2025-06-11 14:29:40 +0100

    Decrease sleep time for AFSK mode

--- main.c
+++ main.c
@@ -982,7 +982,7 @@ int main(int argc, char *argv[])
     if ((mode == AFSK) || (mode == CW))
     {
       get_tlm();
-      sleep(25);
+      sleep(10);
       //      fprintf(stderr, "INFO: Sleeping for 25 sec\n");
 
       int rand_sleep = (int)rnd_float(0.0, 5.0);

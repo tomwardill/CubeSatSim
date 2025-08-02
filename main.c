a116bbf1d90919bf67df010d9e799bad884f1598
Tom Wardill <tom@howrandom.net>
2025-08-02 11:37:13 +0100

    Enable extended telem even in C&C mode

--- main.c
+++ main.c
@@ -1279,7 +1279,9 @@ void get_tlm(void)
                 voltage[map[PLUS_Z]], voltage[map[MINUS_Z]], voltage[map[BAT]]);
 
       else
-        sprintf(tlm_str, "BAT %4.2f %5.1f C ", batteryVoltage, batteryCurrent);
+        //sprintf(tlm_str, "BAT %4.2f %5.1f C ", batteryVoltage, batteryCurrent);
+        sprintf(tlm_str, "BAT %4.2f %5.1f C VOL %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f ", voltage[map[BAT]], current[map[BAT]], voltage[map[PLUS_X]], voltage[map[MINUS_X]], voltage[map[PLUS_Y]], voltage[map[MINUS_Y]],
+                voltage[map[PLUS_Z]], voltage[map[MINUS_Z]], voltage[map[BAT]]);
 
       strcat(str, tlm_str);
     }
